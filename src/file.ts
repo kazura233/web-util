414845afea5174502da64730e9a28f936a533c5f
kazura233 <kazira233@gmail.com>
2021-06-25 18:32:59 +0800

    update

--- src/file.ts
+++ src/file.ts
@@ -14,6 +14,16 @@ export const base64ToArrayBufferView = (data: string) => {
 }
 
 /**
+ * DataURL转Uint8Array
+ * @param data
+ * @returns
+ */
+export const dataURLToArrayBufferView = (data: string) => {
+  const [, base64] = data.split(',')
+  return base64ToArrayBufferView(base64)
+}
+
+/**
  * Uint8Array转Blob
  * @param u8arr
  * @param mime
@@ -21,6 +31,27 @@ export const base64ToArrayBufferView = (data: string) => {
  */
 export const arrayBufferViewToBlob = (u8arr: Uint8Array, mime: string) => {
   return new Blob([u8arr], { type: mime })
+}
+
+/**
+ * Base64转Blob
+ * @param data
+ * @param mime
+ * @returns
+ */
+export const base64ToBlob = (data: string, mime: string) => {
+  return arrayBufferViewToBlob(base64ToArrayBufferView(data), mime)
+}
+
+/**
+ * DataURL转Blob
+ * @param data
+ * @returns
+ */
+export const dataURLToBlob = (data: string) => {
+  const [head, base64] = data.split(',')
+  const mime = head.match(/(?<=:).*?(?=;)/)![0]
+  return base64ToBlob(base64, mime)
 }
 
 /**
@@ -42,4 +73,35 @@ export const arrayBufferViewToFile = (u8arr: Uint8Array, fileName: string, mime:
  */
 export const blobToFile = (blob: Blob, fileName: string) => {
   return new File([blob], fileName, { type: blob.type })
+}
+
+/**
+ * File转Blob
+ * @param file
+ * @returns
+ */
+export const fileToBlob = (file: File) => {
+  return new Blob([file])
+}
+
+/**
+ * Blob转Base64
+ * @param blob
+ * @returns
+ */
+export const blobToBase64 = (blob: Blob) => {
+  return new Promise<string>((resolve) => {
+    const reader = new FileReader()
+    reader.onloadend = (event: ProgressEvent<FileReader>) => resolve(event.target!.result as string)
+    reader.readAsDataURL(blob)
+  })
+}
+
+/**
+ * File转Base64
+ * @param file
+ * @returns
+ */
+export const fileToBase64 = (file: File) => {
+  return blobToBase64(fileToBlob(file))
 }
